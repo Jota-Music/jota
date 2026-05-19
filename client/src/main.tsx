041647990f02baafa919e9459fcf1c14812c88b4
salvadorsru <104313834+salvadorsru@users.noreply.github.com>
2026-05-19 08:28:20 +0200

    refactor(ui): move router out of pages/ to ui/

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import '@/lib/shared/api/socket'
 import '@/lib/shared/views/stores/error-listeners'
 import '@/lib/shared/views/ui/hooks/tabs'
 import { syncAuth } from '@/lib/auth/views/stores/session'
-import Router from '@/lib/shared/views/ui/pages/router'
+import Router from '@/lib/shared/views/ui/router'
 import { render } from 'preact'
 import './style.tw.css'
 
