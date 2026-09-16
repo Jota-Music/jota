7754cb8d35988f6e5cdfe377f508ed51370b5592
salvadorsru <104313834+salvadorsru@users.noreply.github.com>
2026-09-16 02:05:21 +0200

    refactor(frontend): simplify cn helper

--- frontend/src/lib/shared/utils/tw.ts
+++ frontend/src/lib/shared/utils/tw.ts
@@ -1,9 +1,3 @@
 export function cn(...classes: Array<string | false | null | undefined>) {
-	const to_join: string[] = [];
-
-	for (const cls of classes) {
-		if (typeof cls === "string") to_join.push(cls);
-	}
-
-	return to_join.join(" ");
+	return classes.filter(Boolean).join(" ");
 }
